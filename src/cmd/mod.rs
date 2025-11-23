73a366a94bb8594590a3b7fe564c128367318240
Armin Graf <arminhammer@gmail.com>
2025-11-23 09:48:56 -0500

    feat: add run command

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,5 +1,5 @@
-// pub mod run;
+pub mod run;
 pub mod visualize;
 
-// pub use run::{RunArgs, handle_run};
+pub use run::{RunArgs, handle_run};
 pub use visualize::{VisualizeArgs, handle_visualize};
