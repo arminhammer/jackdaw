8a530b0eaf537620b968400d94d700143fa0d96b
Armin Graf <arminhammer@gmail.com>
2025-11-23 09:10:06 -0500

    feat: pyh=thon executor

--- src/providers/executors/mod.rs
+++ src/providers/executors/mod.rs
@@ -1,8 +1,8 @@
-// mod python;
+mod python;
 mod rest;
 mod openapi;
 // mod typescript;
-// pub use python::PythonExecutor;
+pub use python::PythonExecutor;
 pub use rest::RestExecutor;
 pub use openapi::OpenApiExecutor;
 // pub use typescript::TypeScriptExecutor;
